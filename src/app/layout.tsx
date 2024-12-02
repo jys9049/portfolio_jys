9e9b038abce5a83fbf9d8e97e389b69215eebcb3
jys9049 <github.com.com/jys9049>
2024-12-02 16:43:55 +0900

    feat: css 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.scss';
+import '@/styles/_variables.scss';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import styles from './page.module.scss';
 
 export default function Home() {
-  return <div className={styles.page} />;
+  return (
+    <div className={styles.page}>
+      <p className={styles.title}>안녕하세요</p>
+      <p className={styles.subTitle}>반갑습니다</p>
+    </div>
+  );
 }
