b921f416492e9c014a27422d1e966618c123f9fe
jys9049 <github.com.com/jys9049>
2024-12-19 10:23:38 +0900

    [ main-page ] setting: 프로젝트 세팅 - eslint 설정 - jest 라이브러리 추가 및 설정 - testing-library 라이브러리 추가 - svgr 라이브러리 추가 및 설정 - dayjs 라이브러리 추가 - test 관련 스크립트 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,32 @@
+// next.config.js
 const nextConfig = {
-  /* config options here */
+  webpack(config) {
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg'),
+    );
+
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        use: ['@svgr/webpack'],
+      },
+    );
+
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config; // 수정된 config 반환
+  },
 };
 
 export default nextConfig;
