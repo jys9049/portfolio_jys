8a33b96cb4cdefa3a2da2f1d3894c4f73ebebf2d
jys9049 <github.com.com/jys9049>
2024-12-19 11:49:08 +0900

    [ main-page ] feat: ContentsContainer 컴포넌트

---
+++ src/components/Education/const.ts
@@ -0,0 +1,19 @@
+export const educationAndCareerData = [
+  {
+    type: 'EDUCATION',
+    title: '장안대학교 인터넷정보통신과',
+    period: '2016.02 ~ 2021.03',
+  },
+  {
+    type: 'EDUCATION',
+    title: '제로베이스 프론트엔드 스쿨 4기',
+    period: '2022.04 - 2022.11',
+    desc: '우수 수강생 선발',
+  },
+  {
+    type: 'CAREER',
+    title: '블로코 엑스와이지',
+    period: '2023.02 - ',
+    desc: '총 7번의 프로젝트 진행',
+  },
+];
